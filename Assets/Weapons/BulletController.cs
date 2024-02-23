7495f717152e21eff1f61720228f563584702445
Carlos Garcia <cgroig@minsait.com>
2024-02-23 21:30:46 +0100

    feat: fountain configured

---
+++ Assets/Weapons/BulletController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    public float liveTime_;
+    public GameObject particlesPrefab_;
+    Vector3 particlesScale_;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject go = Instantiate<GameObject>(particlesPrefab_, gameObject.transform.position, gameObject.transform.rotation);
+        go.GetComponent<ParticleSystem>().Play();
+
+        go.transform.SetParent(gameObject.transform);
+        Destroy(gameObject,liveTime_);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
