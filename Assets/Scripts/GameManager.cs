2ead5888a91892a95746837c24ca9af7b96f6f9e
Carlos Garcia <cgroig@minsait.com>
2024-03-01 00:39:22 +0100

    feat: avoid duplicate spawn targets

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,6 +6,7 @@ public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
     public static GameManager instance = null;
+    private int lastSpawnSelected_;
     public List<Transform> spawnTRs_ = new List<Transform>();
     public GameObject targetPrefab_;
     public GameObject player_;
@@ -23,13 +24,17 @@ public class GameManager : MonoBehaviour
     }
     void Start()
     {
+        lastSpawnSelected_ = 0;
         spawnTarget();
     }
 
     public void spawnTarget(){
-        int spawnSelected_ = Random.Range(0,spawnTRs_.Count-1);
+        int spawnSelected_;
+        do{
+            spawnSelected_ = Random.Range(0,spawnTRs_.Count-1);
+        }while(spawnSelected_ == lastSpawnSelected_);
         GameObject go_ = Instantiate<GameObject>(targetPrefab_, spawnTRs_[spawnSelected_].position, spawnTRs_[spawnSelected_].rotation);
-
         go_.transform.LookAt(player_.transform);
+        lastSpawnSelected_ = spawnSelected_;
     }
 }
